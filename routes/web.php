c4af78351cb77a5e4258f2d0eb480ba08bf7776c
ArturDymkowski <arturdymkowski@wp.pl>
2024-11-11 10:50:36 +0100

    add start template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'pets', 'as' => 'pets'], function(){
+    Route::get('/{pet}', [PetController::class, 'index'])->name('.index');
 });
